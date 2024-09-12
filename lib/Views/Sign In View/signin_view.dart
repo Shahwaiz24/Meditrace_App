22cb1be73159cc0b6d4c41e9ed0a42d5a93c936f
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-09-12 11:59:43 -0700

    Sign In Completed & Sign Up Started

--- lib/Views/Sign In View/signin_view.dart
+++ lib/Views/Sign In View/signin_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meditrace_project/Components/button.dart';
 import 'package:meditrace_project/Components/text_fields.dart';
 import 'package:meditrace_project/Services/utils.dart';
 import 'package:meditrace_project/Views/Sign%20In%20View/signin_viewmodel.dart';
@@ -21,8 +22,8 @@ class SigninView extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.only(
             top: screenHeight * 0.100,
-            left: screenWidth * 0.050,
-            right: screenWidth * 0.050),
+            left: screenWidth * 0.070,
+            right: screenWidth * 0.070),
         child: Column(
           children: [
             Row(
@@ -42,10 +43,24 @@ class SigninView extends StatelessWidget {
                       ),
                       Align(
                         alignment: Alignment.centerLeft,
-                        child: Icon(
-                          size: screenHeight * 0.030,
-                          Icons.arrow_back_ios_new,
-                          color: AppColors.ButtonBackColor,
+                        child: InkWell(
+                          onTap: () {
+                            model.isPasswordVisible = false;
+                            model.isUiFieldsFill = false;
+                            model.isHiddenPassword = false;
+                            model.isSignInStart = false;
+                            model.isSignUpError = false;
+                            emailController.clear();
+                            passwordController.clear();
+                            model.isFocusEmail = false;
+                            model.isHiddenPassword = true;
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            size: screenHeight * 0.030,
+                            Icons.arrow_back_ios_new,
+                            color: AppColors.ButtonBackColor,
+                          ),
                         ),
                       ),
                     ],
@@ -61,7 +76,11 @@ class SigninView extends StatelessWidget {
                   Consumer<SigninViewmodel>(builder: (context, model, child) {
                     return TextFields(
                         isHidden: false,
-                        onChanged: () {},
+                        onChanged: () {
+                          model.onChangedFocusOFUi(
+                              emailText: emailController.text,
+                              PasswordText: passwordController.text);
+                        },
                         isSuffix: false,
                         Suffix: Text(''),
                         Prefix: model.isFocusEmail == true
@@ -79,23 +98,32 @@ class SigninView extends StatelessWidget {
                         enablefillColor:
                             AppColors.unFocusPrimaryColor.withOpacity(0.1),
                         focusfillColor: AppColors.TextwhiteColor,
-                        outlineColor:
-                            AppColors.unFocusPrimaryColor.withOpacity(0.5),
+                        outlineColor: model.isSignUpError
+                            ? Colors.red.withOpacity(0.5)
+                            : AppColors.unFocusPrimaryColor.withOpacity(0.5),
                         radius: screenHeight * 0.010,
-                        HintText: 'Email Address',
+                        HintText: model.isSignUpError
+                            ? 'Invalid Email'
+                            : 'Email Address',
                         hintStyle: TextStyle(
-                            color: Colors.grey,
+                            color: model.isSignUpError
+                                ? Colors.red.withOpacity(0.5)
+                                : Colors.grey,
                             fontFamily: 'Poppins Regular',
                             fontSize: screenHeight * 0.018));
                   }),
                   SizedBox(
-                    height: screenHeight * 0.020,
+                    height: screenHeight * 0.030,
                   ),
                   Consumer<SigninViewmodel>(builder: (context, model, child) {
                     return TextFields(
                         isHidden: model.isHiddenPassword,
                         onChanged: () {
                           model.onChanged(controller: passwordController);
+                          model.onChangedFocusOFUi(
+                              emailText: emailController.text,
+                              PasswordText: passwordController.text);
+                              
                         },
                         isSuffix: model.isPasswordVisible,
                         Suffix: passwordController.text.isEmpty
@@ -135,12 +163,17 @@ class SigninView extends StatelessWidget {
                         enablefillColor:
                             AppColors.unFocusPrimaryColor.withOpacity(0.1),
                         focusfillColor: AppColors.TextwhiteColor,
-                        outlineColor:
-                            AppColors.unFocusPrimaryColor.withOpacity(0.5),
+                        outlineColor: model.isSignUpError
+                            ? Colors.red.withOpacity(0.5)
+                            : AppColors.unFocusPrimaryColor.withOpacity(0.5),
                         radius: screenHeight * 0.010,
-                        HintText: 'Password',
+                        HintText: model.isSignUpError
+                            ? 'Invalid Password'
+                            : 'Password',
                         hintStyle: TextStyle(
-                            color: Colors.grey,
+                            color: model.isSignUpError
+                                ? Colors.red.withOpacity(0.5)
+                                : Colors.grey,
                             fontFamily: 'Poppins Regular',
                             fontSize: screenHeight * 0.018));
                   }),
@@ -152,19 +185,85 @@ class SigninView extends StatelessWidget {
                     children: [
                       Padding(
                         padding: EdgeInsets.only(right: screenWidth * 0.020),
-                        child: InkWell(
-                          child: Text(
+                        child: InkWell(child: Consumer<SigninViewmodel>(
+                            builder: (context, model, child) {
+                          return Text(
                             'Forgot password?',
                             style: TextStyle(
-                                color: AppColors.unFocusPrimaryColor,
+                                color: model.isUiFieldsFill
+                                    ? AppColors.PrimaryBlueColor
+                                    : AppColors.unFocusPrimaryColor,
                                 fontFamily: 'Poppins Medium',
                                 fontSize: screenHeight * 0.017),
-                          ),
-                        ),
+                          );
+                        })),
                       )
                     ],
                   ),
-
+                  SizedBox(
+                    height: screenHeight * 0.030,
+                  ),
+                  Consumer<SigninViewmodel>(builder: (context, model, child) {
+                    return InkWell(
+                      onTap: () {
+                        model.signInFunction(
+                            Email: emailController.text,
+                            Password: passwordController.text,
+                            context: context);
+                        emailController.clear();
+                        passwordController.clear();
+                      },
+                      child: ButtonComponent(
+                          screenHeight: screenHeight,
+                          screenWidth: screenWidth,
+                          ButtonHeight: 0.075,
+                          radius: 0.080,
+                          backgroundColor: model.isUiFieldsFill
+                              ? AppColors.PrimaryBlueColor
+                              : AppColors.unFocusPrimaryColor,
+                          child: model.isSignInStart
+                              ? Center(
+                                  child: CircularProgressIndicator(
+                                    color: AppColors.PrimaryBlueColor,
+                                  ),
+                                )
+                              : Center(
+                                  child: Text(
+                                    'Sign In',
+                                    style: TextStyle(
+                                        color: AppColors.TextwhiteColor,
+                                        fontFamily: 'Poppins Semi Bold',
+                                        fontSize: screenHeight * 0.020),
+                                  ),
+                                )),
+                    );
+                  }),
+                  SizedBox(
+                    height: screenHeight * 0.030,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                          onTap: () {},
+                          child: Text(
+                            'Don’t have an account?',
+                            style: TextStyle(
+                                color: AppColors.TextblackColor,
+                                fontFamily: 'Poppins Medium',
+                                fontSize: screenHeight * 0.018),
+                          )),
+                      InkWell(
+                          onTap: () {},
+                          child: Text(
+                            '  Sign up',
+                            style: TextStyle(
+                                color: AppColors.PrimaryBlueColor,
+                                fontFamily: 'Poppins Bold',
+                                fontSize: screenHeight * 0.018),
+                          ))
+                    ],
+                  ),
                 ],
               ),
             )
--- lib/Views/Sign In View/signin_viewmodel.dart
+++ lib/Views/Sign In View/signin_viewmodel.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class SigninViewmodel with ChangeNotifier {
   bool isPasswordVisible = false;
+  bool isSignInStart = false;
+  bool isUiFieldsFill = false;
   bool isHiddenPassword = true;
   bool isFocusEmail = false; // Focus state track karne ke liye
+  bool isSignUpError = false;
   void onEmailFocusChange(bool hasFocus) {
     isFocusEmail = hasFocus;
 
@@ -15,12 +18,27 @@ class SigninViewmodel with ChangeNotifier {
     notifyListeners();
   }
 
+  onChangedFocusOFUi(
+      {required String emailText, required String PasswordText}) {
+    if ((emailText.length >= 5) && (PasswordText.length >= 4)) {
+      isUiFieldsFill = true;
+      notifyListeners();
+    } else {
+      isUiFieldsFill = false;
+      notifyListeners();
+    }
+  }
+
   onChanged({required TextEditingController controller}) {
     if (controller.text.isEmpty) {
       isPasswordVisible = false;
+      isSignUpError = false;
       notifyListeners();
     } else {
       isPasswordVisible = true;
+
+      isSignUpError = false;
+
       notifyListeners();
     }
   }
@@ -30,5 +48,39 @@ class SigninViewmodel with ChangeNotifier {
     isFocusPassword = hasFocus;
 
     notifyListeners();
+  }
+
+  validation({required String EmailText, required String Password}) {
+    if ((EmailText.contains('@') && (EmailText.contains('.com'))) &&
+        (Password.length >= 5)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  signInFunction(
+      {required String Email,
+      required String Password,
+      required BuildContext context}) async {
+    isSignInStart = true;
+    notifyListeners();
+    bool validate = await validation(EmailText: Email, Password: Password);
+    if (validate == true) {
+      print('Navigating and SuccessFully Login');
+
+      // Navigator.pushReplacement(
+      //     context, MaterialPageRoute(builder: (context) => HomeView()));
+      isSignInStart = false;
+      notifyListeners();
+    } else {
+      isSignInStart = false;
+      isSignUpError = true;
+      isUiFieldsFill = false;
+      notifyListeners();
+      await Future.delayed(const Duration(seconds: 3));
+      isSignUpError = false;
+      notifyListeners();
+    }
   }
 }
