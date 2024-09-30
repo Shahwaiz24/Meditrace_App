0d56f7cee96e0179b178eddd87bef58b12a82c37
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-10-01 03:57:39 +0500

    Password SuccessFuly Changed View Started

---
+++ lib/Views/Forgot Password Views/Password Changed View/password_changed_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PasswordChangedView extends StatelessWidget {
+  const PasswordChangedView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
