ad5b79e9db41d4e4f62ff98bd945ce91607f5977
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-10-01 03:55:28 +0500

    Create New Password Screen Completed

--- lib/Views/Forgot Password Views/Create New Password View/create_newpassword_view.dart
+++ lib/Views/Forgot Password Views/Create New Password View/create_newpassword_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meditrace_project/Components/button.dart';
 import 'package:meditrace_project/Components/text_fields.dart';
 import 'package:meditrace_project/Services/utils.dart';
 import 'package:meditrace_project/Views/Forgot%20Password%20Views/Create%20New%20Password%20View/create_newpassword_viewmodel.dart';
@@ -57,62 +58,224 @@ class CreateNewpasswordView extends StatelessWidget {
             SizedBox(
               height: screenHeight * 0.040,
             ),
-            TextFields(
-                controller: passwordController,
-                enablefillColor: Colors.black,
-                focusfillColor: model.isFocusPassword == true
-                    ? AppColors.TextwhiteColor
-                    : model.isPasswordNotEmpty == true
-                        ? AppColors.TextwhiteColor
-                        : AppColors.unFocusPrimaryColor.withOpacity(0.1),
-                outlineColor: model.isError == true
-                    ? Colors.red
-                    : AppColors.unFocusPrimaryColor.withOpacity(0.5),
-                radius: screenWidth * 0.020,
-                isHidden: false,
-                HintText: "Enter Password",
-
-              
-                hintStyle: TextStyle(
-                  color: AppColors.unFocusPrimaryColor,
-
-                  //  model.isError
-                  //     ? Colors.red
-                  //     : AppColors.unFocusPrimaryColor,
-
-                  fontFamily: 'Poppins Regular',
+            Consumer<CreateNewpasswordViewmodel>(
+                builder: (context, model, child) {
+              return TextFields(
+                  controller: passwordController,
+                  enablefillColor: Colors.black,
+                  focusfillColor: model.isFocusPassword == true
+                      ? AppColors.TextwhiteColor
+                      : model.isPasswordNotEmpty == true
+                          ? AppColors.TextwhiteColor
+                          : AppColors.unFocusPrimaryColor.withOpacity(0.1),
+                  outlineColor: model.isError == true
+                      ? Colors.red
+                      : AppColors.unFocusPrimaryColor.withOpacity(0.5),
+                  radius: screenWidth * 0.020,
+                  isHidden: model.isPasswordHidden,
+                  HintText: "Enter Password",
+                  hintStyle: TextStyle(
+                    color: model.isError
+                        ? Colors.red
+                        : AppColors.unFocusPrimaryColor,
+                    fontFamily: 'Poppins Regular',
+                  ),
+                  Prefix: model.isFocusPassword
+                      ? const ImageIcon(
+                          AssetImage("assets/images/icon/password_icon.png"),
+                          color: Colors.black,
+                        )
+                      : model.isPasswordNotEmpty == true
+                          ? const ImageIcon(
+                              AssetImage(
+                                  "assets/images/icon/password_icon.png"),
+                              color: Colors.black,
+                            )
+                          : ImageIcon(
+                              const AssetImage(
+                                  "assets/images/icon/password_icon.png"),
+                              color: AppColors.unFocusPrimaryColor
+                                  .withOpacity(0.8),
+                              size: screenHeight * 0.032,
+                            ),
+                  Suffix: model.isPasswordNotEmpty == false
+                      ? const Text('')
+                      : Padding(
+                          padding: EdgeInsets.only(right: screenWidth * 0.030),
+                          child: InkWell(
+                            onTap: () {
+                              model.hiddenText();
+                            },
+                            child: model.isPasswordHidden == true
+                                ? const Icon(
+                                    Icons.visibility_off_outlined,
+                                    color: Colors.black,
+                                  )
+                                : const Icon(
+                                    Icons.visibility_outlined,
+                                    color: Colors.black,
+                                  ),
+                          ),
+                        ),
+                  isSuffix: true,
+                  isNumberKeyboard: false,
+                  isPrefix: true,
+                  onFocus: (value) {
+                    model.isPasswordFocus(focus: value);
+                  },
+                  contentStyle: const TextStyle(
+                      fontFamily: 'Poppins Regular',
+                      fontWeight: FontWeight.w500),
+                  onChanged: (value) {
+                    model.isPasswordEmptyCheck(
+                        password: passwordController.text);
+                    model.onChangedFunction(
+                        firstPassword: passwordController.text,
+                        repeatpassword: repeatPasswordController.text);
+                  });
+            }),
+            SizedBox(
+              height: screenHeight * 0.020,
+            ),
+            Consumer<CreateNewpasswordViewmodel>(
+                builder: (context, model, child) {
+              return TextFields(
+                  controller: repeatPasswordController,
+                  enablefillColor: Colors.black,
+                  focusfillColor: model.isFocusRepeatPassword == true
+                      ? AppColors.TextwhiteColor
+                      : model.isRepeatPasswordNotEmpty == true
+                          ? AppColors.TextwhiteColor
+                          : AppColors.unFocusPrimaryColor.withOpacity(0.1),
+                  outlineColor: model.isError == true
+                      ? Colors.red
+                      : AppColors.unFocusPrimaryColor.withOpacity(0.5),
+                  radius: screenWidth * 0.020,
+                  isHidden: model.isPasswordHidden,
+                  HintText: "Confirm Password",
+                  hintStyle: TextStyle(
+                    color: model.isError
+                        ? Colors.red
+                        : AppColors.unFocusPrimaryColor,
+                    fontFamily: 'Poppins Regular',
+                  ),
+                  Prefix: model.isFocusRepeatPassword
+                      ? const ImageIcon(
+                          AssetImage("assets/images/icon/password_icon.png"),
+                          color: Colors.black,
+                        )
+                      : model.isRepeatPasswordNotEmpty == true
+                          ? const ImageIcon(
+                              AssetImage(
+                                  "assets/images/icon/password_icon.png"),
+                              color: Colors.black,
+                            )
+                          : ImageIcon(
+                              const AssetImage(
+                                  "assets/images/icon/password_icon.png"),
+                              color: AppColors.unFocusPrimaryColor
+                                  .withOpacity(0.8),
+                              size: screenHeight * 0.032,
+                            ),
+                  Suffix: model.isRepeatPasswordNotEmpty == false
+                      ? const Text('')
+                      : Padding(
+                          padding: EdgeInsets.only(right: screenWidth * 0.030),
+                          child: InkWell(
+                            onTap: () {
+                              model.hiddenText();
+                            },
+                            child: model.isPasswordHidden == true
+                                ? const Icon(
+                                    Icons.visibility_off_outlined,
+                                    color: Colors.black,
+                                  )
+                                : const Icon(
+                                    Icons.visibility_outlined,
+                                    color: Colors.black,
+                                  ),
+                          ),
+                        ),
+                  isSuffix: true,
+                  isNumberKeyboard: false,
+                  isPrefix: true,
+                  onFocus: (value) {
+                    model.isPasswordRepeatFocus(focus: value);
+                  },
+                  contentStyle: const TextStyle(
+                      fontFamily: 'Poppins Regular',
+                      fontWeight: FontWeight.w500),
+                  onChanged: (value) {
+                    model.isRepeatPasswordEmptyCheck(
+                        passwordRepeat: repeatPasswordController.text);
+                    model.onChangedFunction(
+                        firstPassword: passwordController.text,
+                        repeatpassword: repeatPasswordController.text);
+                  });
+            }),
+            const Spacer(),
+            Consumer<CreateNewpasswordViewmodel>(
+                builder: (context, model, child) {
+              return Padding(
+                padding: EdgeInsets.only(bottom: screenHeight * 0.030),
+                child: Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        if (model.isStart == false) {
+                          model.onChangedPasswordTap(
+                              password: passwordController.text,
+                              repeatPassword: repeatPasswordController.text,
+                              context: context);
+                          passwordController.clear();
+                          repeatPasswordController.clear();
+                        }
+                      },
+                      child: ButtonComponent(
+                          screenHeight: screenHeight,
+                          screenWidth: screenWidth,
+                          ButtonHeight: 0.075,
+                          decoration: BoxDecoration(
+                              color: model.isFieldsFill == true
+                                  ? AppColors.PrimaryBlueColor
+                                  : AppColors.unFocusPrimaryColor,
+                              borderRadius:
+                                  BorderRadius.circular(screenWidth * 0.080)),
+                          child: Center(
+                            child: model.isStart == true
+                                ? CircularProgressIndicator(
+                                    color: AppColors.TextwhiteColor,
+                                  )
+                                : Text(
+                                    'Change Password',
+                                    style: TextStyle(
+                                        color: AppColors.TextwhiteColor,
+                                        fontFamily: 'Poppins Semi Bold',
+                                        fontSize: screenHeight * 0.020),
+                                  ),
+                          )),
+                    ),
+                    SizedBox(height: screenHeight * 0.010),
+                    InkWell(
+                      onTap: () {
+                        if (model.isStart == false) {
+                          passwordController.clear();
+                          repeatPasswordController.clear();
+                          model.navigatetoBack(context: context);
+                        }
+                      },
+                      child: Text(
+                        'Go Back',
+                        style: TextStyle(
+                            color: AppColors.PrimaryBlueColor,
+                            fontFamily: "Poppins Bold",
+                            fontSize: screenHeight * 0.018),
+                      ),
+                    )
+                  ],
                 ),
-                Prefix: Icon(
-                  Icons.lock_outline_rounded,
-                  color: AppColors.unFocusPrimaryColor.withOpacity(0.8),
-                  size: screenHeight * 0.032,
-                ),
-
-                //  model.isFocusPassword
-                //             ? const Icon(
-                //                 Icons.lock_outline_rounded,
-                //                 color: Colors.black,
-                //               )
-                //             : model.isPasswordNotEmpty == true ? const Icon(
-                //                 Icons.lock_outline_rounded,
-                //                 color: Colors.black,
-                //               )  : Icon(
-                //                 Icons.lock_outline_rounded,
-                //                 color: AppColors.unFocusPrimaryColor
-                //                     .withOpacity(0.8),
-                //                 size: screenHeight * 0.032,
-                //               ),
-                Suffix: const Icon(
-                  Icons.visibility_off_outlined,
-                  color: Colors.black,
-                ),
-                isSuffix: true,
-                isNumberKeyboard: false,
-                isPrefix: true,
-                onFocus: (value) {},
-                contentStyle: const TextStyle(
-                    fontFamily: 'Poppins Regular', fontWeight: FontWeight.w500),
-                onChanged: (value) {})
+              );
+            })
           ],
         ),
       ),
--- lib/Views/Forgot Password Views/Create New Password View/create_newpassword_viewmodel.dart
+++ lib/Views/Forgot Password Views/Create New Password View/create_newpassword_viewmodel.dart
@@ -6,4 +6,96 @@ class CreateNewpasswordViewmodel with ChangeNotifier {
   bool isPasswordNotEmpty = false;
   bool isRepeatPasswordNotEmpty = false;
   bool isError = false;
+  bool isStart = false;
+  bool isFieldsFill = false;
+
+  bool isPasswordHidden = false;
+
+  String finalPassword = "";
+
+  void isPasswordFocus({required bool focus}) {
+    isFocusPassword = focus;
+    notifyListeners();
+  }
+
+  void isPasswordRepeatFocus({required bool focus}) {
+    isFocusRepeatPassword = focus;
+    notifyListeners();
+  }
+
+  hiddenText() {
+    isPasswordHidden = !isPasswordHidden;
+    notifyListeners();
+  }
+
+  isRepeatPasswordEmptyCheck({required String passwordRepeat}) {
+    if (passwordRepeat.isNotEmpty) {
+      isRepeatPasswordNotEmpty = true;
+      notifyListeners();
+    } else {
+      isRepeatPasswordNotEmpty = false;
+      notifyListeners();
+    }
+  }
+
+  isPasswordEmptyCheck({required String password}) {
+    if (password.isNotEmpty) {
+      isPasswordNotEmpty = true;
+      notifyListeners();
+    } else {
+      isPasswordNotEmpty = false;
+      notifyListeners();
+    }
+  }
+
+  onChangedFunction(
+      {required String firstPassword, required String repeatpassword}) {
+    if ((firstPassword.isNotEmpty && firstPassword == repeatpassword) &&
+        (repeatpassword.isNotEmpty && repeatpassword == firstPassword)) {
+      isFieldsFill = true;
+      notifyListeners();
+    } else {
+      isFieldsFill = false;
+      notifyListeners();
+    }
+  }
+
+  onChangedPasswordTap(
+      {required String password,
+      required String repeatPassword,
+      required BuildContext context}) async {
+    isStart = true;
+    isFieldsFill = false;
+    notifyListeners();
+    if ((password.isNotEmpty &&
+            password.length >= 4 &&
+            password == repeatPassword) &&
+        (repeatPassword.isNotEmpty &&
+            repeatPassword.length >= 4 &&
+            repeatPassword == repeatPassword)) {
+      print("Everythings Alright Api Work Here");
+      await Future.delayed(Duration(seconds: 1));
+      isStart = false;
+      notifyListeners();
+    } else {
+      isStart = false;
+      isError = true;
+      notifyListeners();
+      await Future.delayed(Duration(seconds: 2));
+      isError = false;
+      notifyListeners();
+    }
+  }
+
+  void navigatetoBack({required BuildContext context}) {
+    isStart = false;
+    isPasswordHidden = false;
+    isFocusRepeatPassword = false;
+    isRepeatPasswordNotEmpty = false;
+    isPasswordNotEmpty = false;
+    isFocusRepeatPassword = false;
+    isError = false;
+    isFieldsFill = false;
+    Navigator.pop(context);
+  }
 }
