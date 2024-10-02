40b747ee16c86a7916fabd02240a5e3df874e554
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-10-02 11:05:58 -0700

    Start Testing the Back-Ground Services

--- lib/Services/back_ground_service.dart
+++ lib/Services/back_ground_service.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart'; // Required for Android specific service
 
-
 class BackGroundService {
-
-  static  Future<bool> onStart(ServiceInstance service) async {
-    Timer.periodic(const Duration(minutes: 5), (timer) async {
+  static Future<bool> onStart(ServiceInstance service) async {
+    Timer.periodic(const Duration(seconds: 3), (timer) async {
       // Perform your periodic task here
       print("Background service running at ${DateTime.now()}");
     });
@@ -15,7 +13,7 @@ class BackGroundService {
   }
 
 // Initialize the background service
-static  Future<void> initializeService() async {
+  static Future<void> initializeService() async {
     final service = FlutterBackgroundService();
 
     await service.configure(
@@ -30,9 +28,9 @@ static  Future<void> initializeService() async {
       ),
       iosConfiguration: IosConfiguration(
         autoStart: true,
-        onBackground: onStart, 
+        onBackground: onStart,
       ),
     );
+    print("Services Configured");
   }
-
 }
